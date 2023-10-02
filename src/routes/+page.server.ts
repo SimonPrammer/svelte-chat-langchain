e8846c965a04b5437e4df9944a078b0f12aca22b
Simon Prammer <simon.prammer@gmx.net>
2023-10-02 09:17:41 +0900

    env var checks

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,23 @@
 import { sql } from '@vercel/postgres';
 
 export const load = async (event) => {
-	const exists = await sql`
-			SELECT EXISTS (
-				SELECT FROM information_schema.tables 
-				WHERE table_schema = 'public'
-				AND table_name = 'langchain_docs_embeddings'
-			);
-		`;
-	return {
-		vectorDbExists: exists?.rows[0]?.exists
-	};
+	const isEnvSet = !!process.env.POSTGRES_URL && !!process.env.OPENAI_API_KEY;
+
+	if (isEnvSet) {
+		const exists = await sql`
+		SELECT EXISTS (
+			SELECT FROM information_schema.tables 
+			WHERE table_schema = 'public'
+			AND table_name = 'langchain_docs_embeddings'
+		);
+	`;
+		return {
+			isEnvSet,
+			vectorDbExists: exists?.rows[0]?.exists
+		};
+	} else {
+		return {
+			isEnvSet
+		};
+	}
 };
